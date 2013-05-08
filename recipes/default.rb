16bf5f7282061a638f9b65fcc1cf6ac4424fb799
Mikhail Stolbov <mstolbov@gmail.com>
2013-05-08 21:03:15 +0400

    Add create_if_missing attribute to remote fetch. Bump version to 3.2.3

--- recipes/default.rb
+++ recipes/default.rb
@@ -25,9 +25,9 @@ zeromq_tar_gz = File.join(Chef::Config[:file_cache_path], "/", "zeromq-#{node[:z
 
 remote_file zeromq_tar_gz do
   source node[:zeromq][:src_mirror]
+  action :create_if_missing
 end
 
-package "libtool"
 package "uuid-dev" do
   action :upgrade
 end
